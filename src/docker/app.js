5d7605eeaa01058710b862991b4e4d4c45f08009
Yaroslav Panchishyn <yarpan@gmail.com>
2025-04-16 10:34:50 +0300

    docker

--- src/docker/app.js
+++ src/docker/app.js
@@ -2,12 +2,11 @@
 // The Docker container must have Node.js installed, and the web application must be available on a specific port.
 
 const http = require('http');
-
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, Docker!\n');
+    res.end('Hello from Docker!\n');
 });
 
 server.listen(PORT, () => {
